3139863f9f139faa1e7d5c693946fa031c6277d7
Julien <julien.moulis@moulis.me>
2018-10-20 12:55:55 +0200

    Env

--- back/index.js
+++ back/index.js
@@ -1,8 +1,11 @@
-const app = require('express')();
-const server = require('http').Server(app);
+const express = require('express');
+const fs = require('fs');
+require('dotenv').config();
+const http = require('http');
+const https = require('https');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const io = require('socket.io')(server);
+const SocketIO = require('socket.io');
 const mainSocket = require('./socket');
 
 mongoose.Promise = global.Promise;
@@ -17,7 +20,55 @@ db.once('open', () => {
   console.log('database connected');
 });
 
+const app = express();
+let server;
+if (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'test') {
+  server = http.createServer(app);
+} else {
+  const httpsOptions = {
+    key: fs.readFileSync(process.env.HTTPS_KEY),
+    cert: fs.readFileSync(process.env.HTTPS_CERT),
+  };
+  server = https.createServer(httpsOptions, app);
+}
+
+const io = new SocketIO(server);
 app.use(cors());
 mainSocket(io);
 
 server.listen(3500);
+
+// const app = require('express')();
+// const server = require('http').Server(app);
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// const io = require('socket.io')(server);
+// const mainSocket = require('./socket');
+
+// mongoose.Promise = global.Promise;
+
+// mongoose.connect(
+//   'mongodb://localhost/shopping',
+//   { useNewUrlParser: true },
+// );
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', () => {
+//   console.log('database connected');
+// });
+
+// let server;
+// if (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'test') {
+//   server = http.createServer(app);
+// } else {
+//   const httpsOptions = {
+//     key: fs.readFileSync(config.key_url),
+//     cert: fs.readFileSync(config.cert_url),
+//   };
+//   server = https.createServer(httpsOptions, app);
+// }
+
+// app.use(cors());
+// mainSocket(io);
+
+// server.listen(3500);
